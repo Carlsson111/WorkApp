57c8904c137052e2110aac9dbf1bca70f85d958c
Carlsson111 <Linusjaa1@gmail.com>
2024-11-15 08:52:02 +0100

    Added TodoItemDAO and Collection for it

---
+++ src/main/java/se/lexicon/dao/TodoItemDAO.java
@@ -0,0 +1,22 @@
+package se.lexicon.dao;
+
+import se.lexicon.TodoItem;
+
+
+import java.time.LocalDate;
+import java.util.Collection;
+;
+
+public interface TodoItemDAO {
+    void persist (TodoItem todoItem);
+    TodoItem findById;
+    Collection<TodoItem> findAll();
+    Collection<TodoItem> findAllByDoneStatus(boolean done);
+    Collection<TodoItem> findByTitleContains(String title);
+    Collection<TodoItem> findByPersonId(int personId);
+    Collection<TodoItem>findByDeadlineBefore(LocalDate deadline);
+    Collection<TodoItem> findByDeadlineAfter(LocalDate deadline);
+    void remove (int id);
+
+
+}
