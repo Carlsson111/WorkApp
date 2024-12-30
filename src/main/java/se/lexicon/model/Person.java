f8926c244487421b9868b2784f545cdfe303a3c3
Carlsson111 <Linusjaa1@gmail.com>
2024-12-30 09:27:13 +0100

    Person Class fixed up for jdbc

--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -5,52 +5,39 @@ package se.lexicon.model;
 import java.util.Objects;
 
 public class Person {
+
+
     private int id;
     private String firstName;
     private String lastName;
-    private String email;
-    private AppUser credentials;
 
-    public Person(String firstName, String lastName, String email) {
-        this(1, firstName, lastName, email);
 
-    }
 
-    public Person(int id, String firstName, String lastName, String email) {
+
+
+    public Person(int id, String firstName, String lastName) {
         if (firstName == null || firstName.isEmpty()) {
             throw new IllegalArgumentException("First name cannot be null or empty");
         }
         if (lastName == null || lastName.isEmpty()) {
             throw new IllegalArgumentException("Last name cannot be null or empty");
         }
-        if (email == null || email.isEmpty()) {
-            throw new IllegalArgumentException("Email cannot be null or empty");
-        }
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
-    }
 
+    }
 
     public int getId() {
         return id;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
@@ -60,6 +47,10 @@ public class Person {
         this.firstName = firstName;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
     public void setLastName(String lastName) {
         if (lastName == null || lastName.isEmpty()) {
             throw new IllegalArgumentException("Last name cannot be null or empty");
@@ -67,36 +58,24 @@ public class Person {
         this.lastName = lastName;
     }
 
-    public void setEmail(String email) {
-        if (email == null || email.isEmpty()) {
-            throw new IllegalArgumentException("Email cannot be null or empty");
-        }
-        this.email = email;
-    }
-
-    public AppUser getCredentials() {
-        return credentials;
-    }
-
-    public void setCredentials(AppUser credentials) {
-        this.credentials = credentials;
-    }
-
-
+    @Override
     public String toString() {
-        return "Person {id: " + id + ", name: " + firstName + " " + lastName + ", email: " + email + "}"; }
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Person person = (Person) o;
-        return id == person.id && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(email, person.email);
+        return id == person.id && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, email);
+        return Objects.hash(id, firstName, lastName);
     }
-
 }
